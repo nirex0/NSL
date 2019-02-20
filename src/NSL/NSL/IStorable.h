34abb699cefc11653a612a37e9bdcf0c1a9fad13
Nirex0 <Nirex.0@gmail.com>
2019-02-20 15:30:09 +0330

    Fix: IStorable is now in NSL namespace

--- src/NSL/NSL/IStorable.h
+++ src/NSL/NSL/IStorable.h
@@ -8,22 +8,25 @@
 
 typedef long HRESULT;
 
-//-->DOC_CLASS
-// Main Interface for all the classes that need to have saving and loading functionality.
-class NAPI IStorable
+namespace NSL
 {
-public:
-	//-->DOC_FUNC
-	// Store the data within the class in a string
-	// OUT str: put the calculated string inside the str argument
-	// returns: whether or not the operation was successful
-	virtual HRESULT Store(const std::string& OUT str) PURE;
+	//-->DOC_CLASS
+	// Main Interface for all the classes that need to have saving and loading functionality.
+	class NAPI IStorable
+	{
+	public:
+		//-->DOC_FUNC
+		// Store the data within the class in a string
+		// OUT str: put the calculated string inside the str argument
+		// returns: whether or not the operation was successful
+		virtual HRESULT Store(const std::string& OUT str) PURE;
 
-	//-->DOC_FUNC
-	// Retrieves the data from the given string and loads it into the class
-	// IN str: put the string inside the object's members
-	// returns: whether or not the operation was successful
-	virtual HRESULT Retrieve(const std::string& IN str) PURE;
-};
+		//-->DOC_FUNC
+		// Retrieves the data from the given string and loads it into the class
+		// IN str: put the string inside the object's members
+		// returns: whether or not the operation was successful
+		virtual HRESULT Retrieve(const std::string& IN str) PURE;
+	};
+}
 
 #endif // !_I_STORABLE_H_
